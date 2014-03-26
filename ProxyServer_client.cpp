1e77f0b5ecebc1046d5a847525e4fa0bc48cb157
Sandeep Manchem <smanchem3@gatech.edu>
2014-03-26 00:53:26 -0400

    updated IP

--- ProxyServer_client.cpp
+++ ProxyServer_client.cpp
@@ -17,7 +17,7 @@
    using namespace Test;
 
    int main(int argc, char **argv) {
-     boost::shared_ptr<TSocket> socket(new TSocket("localhost", 9090));
+     boost::shared_ptr<TSocket> socket(new TSocket("128.61.121.139", 9090));
      boost::shared_ptr<TTransport> transport(new TBufferedTransport(socket));
      boost::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
 
