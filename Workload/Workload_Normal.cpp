ebdaae36bf0e431d67cbf2bb189bd768614cb96f
SunayanaG <sgali6@gatech.edu>
2014-03-27 16:18:51 -0400

    Update Workload_Normal.cpp

--- Workload/Workload_Normal.cpp
+++ Workload/Workload_Normal.cpp
@@ -6,19 +6,19 @@ using namespace std;
 
 int main()
 {
-  const int nrolls=50;  // number of experiments
-  const int nstars=50;    // maximum number of stars to distribute
+  const int nrolls=100;  // number of experiments
+  const int nstars=100;    // maximum number of stars to distribute
 
   default_random_engine generator;
   normal_distribution<double> distribution(5.0,2.0);
   
-  int p[10]={};
-  int s[50]={};
+  int p[20]={};
+  int s[100]={};
   int j=0;
 
   for (int i=0; i<nrolls; ++i) {
     double number = distribution(generator);
-    if ((number>=0.0)&&(number<10.0)){
+    if ((number>=0.0)&&(number<20.0)){
 	++p[int(number)];
 	s[i] = number;
     }
@@ -26,11 +26,11 @@ int main()
 
   std::cout << "normal_distribution (5.0,2.0):" << std::endl;
 
-  for (int i=0; i<10; ++i) {
+  for (int i=0; i<20; ++i) {
     std::cout << i << "-" << (i+1) << ": ";
     std::cout << std::string(p[i]*nstars/nrolls,'*') << std::endl;
   }
-  for(int i=0; i<50;i++){
+  for(int i=0; i<100;i++){
 	if(s[i] == 0) cout<<"\nhttp://www.hindu.com";
 	if(s[i] == 1) cout<<"\nhttp://www.bbc.com/news";
 	if(s[i] == 2) cout<<"\nhttp://www.theguardian.com/us";
